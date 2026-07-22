e5d003c84cbb767bdf6f2ee9706cadffd1995757
Kaushik-FSD <rockstarkaushik1000@gmail.com>
2026-07-22 15:13:16 +0530

    fixing the issues

--- src/main/java/secondFile.java
+++ src/main/java/secondFile.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class firstFile {
-    public static void main(String[] args) {
+public class secondFile {
+    public static void function2() {
         System.out.println("Second file java!!!");
     }
 }
